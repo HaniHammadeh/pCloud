e07345c38f20c3a753e1a791f8716a64a4bc8fb2
root <root@DESKTOP-BVJ2JL1.localdomain>
2022-12-05 13:07:28 +0400

    first commit

---
+++ pcloud.sh
@@ -0,0 +1,159 @@
+#!/usr/bin/env bash
+#
+# pCloud uploader 
+#
+# Copyright (C) 2022 Hani Hammadeh @hanihammadeh
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+
+CURL_BIN=$(which curl) 
+CURL_OPT=' -s'
+JQ_BIN=$(which jq)
+END_POINT="https://u.pcloud.com"
+API_LOGIN="https://api.pcloud.com/login"
+API_AUTH_URL="${END_POINT}/oauth2"
+API_AUTH_TOKEN_URL="https://api.pcloud.com/oauth2_token"
+API_LIST_FOLDER_URL="https://api.pcloud.com/listfolder"
+CONFIG_FILE="./.pcloud_config"
+VERSION="1.0"
+
+source "${CONFIG_FILE}"
+#CONFIG_FILE has the following parameters:
+##EMAIL_ADDRESS=" Your pCloud Email Address"
+##PASSWORD="Your pCloud Login password"
+##CLIENT_ID=" Your APP pCloud Client ID"
+##CLIENT_SECRET="Your APP pCloud Client Secret"
+
+function usage
+{
+    echo -e "pCloud Uploader v$VERSION"
+    echo -e "Hani Hammadeh - @hanihammadeh\n"
+    echo -e "Usage: $0 [PARAMETERS] COMMAND..."
+    echo -e "\nCommands:"
+    echo -e "\t --list   <REMOTE_DIR_ID> "
+    echo -e "\t --list-remote-dir "
+    echo -e "\t --help   show this help message"
+    echo -en "\nFor more info and examples, please see the README file.\n\n"
+    exit 1
+}
+
+
+function login() {
+$CURL_BIN  $CURL_OPT "${API_LOGIN}" \
+  -H 'Accept: application/json, text/javascript, */*; q=0.01' \
+  -H 'Accept-Language: en-US,en;q=0.9' \
+  -H 'Connection: keep-alive' \
+  -H 'Content-Type: application/x-www-form-urlencoded; charset=UTF-8' \
+  --data-urlencode username="${EMAIL_ADDRESS}" \
+  --data-urlencode password="${PASSWORD}" \
+  --compressed
+}
+
+#login
+
+function get_code_token() {
+$CURL_BIN $CURL_OPT "${END_POINT}/oauth2/authorize?client_id=$CLIENT_ID&response_type=code&auth=$AUTH_ID" \
+  -H 'Accept: text/html,application/xhtml+xml,application/xml' \
+  -H 'Accept-Language: en-US,en;q=0.9' \
+  -H 'Connection: keep-alive' \
+  --compressed
+}
+
+function get_access_token(){
+$CURL_BIN $CURL_OPT --location --request POST "${API_AUTH_TOKEN_URL}" \
+--header 'Content-Type: application/x-www-form-urlencoded' \
+--header 'Accept: application/json' \
+--data-urlencode client_id=$CLIENT_ID \
+--data-urlencode client_secret=$CLIENT_SECRET \
+--data-urlencode code=$1
+
+}
+
+function list_root_folder(){
+
+$CURL_BIN $CURL_OPT --location --request POST "${API_LIST_FOLDER_URL}" \
+--header "Authorization: Bearer $1" \
+--header "Content-Type: application/x-www-form-urlencoded" \
+--data-urlencode folderid=0
+
+}
+
+function pretty_list_root_folder(){
+  list_root_folder $access_token|jq -r '(["PATH","NAME","FOLDERID"]),(.metadata.contents[]|[.path, .name, .folderid])|@tsv'
+}
+
+
+function list_dir(){
+
+curl -s --location --request POST "${API_LIST_FOLDER_URL}" \
+--header "Authorization: Bearer $1" \
+--header "Content-Type: application/x-www-form-urlencoded" \
+--data-urlencode folderid=$2
+
+
+}
+function pretty_list_dir(){
+	list_dir  $access_token $1|jq -r '(["NAME", "FILEID"]),(.metadata.contents[]|[.name, .fileid])|@tsv'
+}
+
+##############################################
+###########START##############################
+##############################################
+
+# get the authntication ID
+AUTH_ID=$(login|jq -r '.auth')
+
+# get the assoociated code
+code=$(echo "$(get_code_token)"|grep 'class="code"'| sed 's/<[^>]*>//g'|head -1)
+
+# get the access token
+access_token=$(get_access_token $code |jq -r '.access_token')
+
+# read cli options
+! getopt --test > /dev/null 
+if [[ ${PIPESTATUS[0]} -ne 4 ]]; then
+    echo 'I’m sorry, `getopt --test` failed in this environment.'
+    exit 1
+fi
+
+LONGOPTS=list-root-dir,list-dir:,help
+OPTIONS=lr:h
+! PARSED=$(getopt --options=$OPTIONS --longoptions=$LONGOPTS --name "$0" -- "$@")
+if [[ ${PIPESTATUS[0]} -ne 0 ]]; then
+    exit 2
+fi
+eval set -- "$PARSED"
+while true; do
+    case "$1" in
+        -l|--list-root-dir)
+            pretty_list_root_folder
+            shift
+            ;;
+        -r|--list-dir)
+            pretty_list_dir $2
+	    #list_dir $access_token $2
+            shift 2
+            ;;
+        -h|--help)
+            usage
+            shift
+            ;;
+        --)
+            shift
+            break
+            ;;
+        *)
+            echo "Error"
+            exit 3
+            ;;
+    esac
+done
