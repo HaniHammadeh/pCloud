b8e8f0f2bbcbb18d5ef0b4b57724a17cff9c0323
hanihammadeh <hani.hammadeh@gmail.com>
2022-12-08 14:06:23 +0400

    added public urls functions

--- pcloud.sh
+++ pcloud.sh
@@ -24,6 +24,8 @@ API_AUTH_URL="${END_POINT}/oauth2"
 API_AUTH_TOKEN_URL="https://api.pcloud.com/oauth2_token"
 API_LIST_FOLDER_URL="https://api.pcloud.com/listfolder"
 API_UPLOAD_FILE="https://api17.pcloud.com/uploadfile"
+API_FILE_PUBLIC_LINK="https://api.pcloud.com/getfilepublink"
+API_LIST_PUBLIC_LINKS="https://api.pcloud.com/listpublinks"
 CONFIG_FILE="./.pcloud_config"
 VERSION="1.0"
 
@@ -136,6 +138,21 @@ $CURL_BIN  -XPOST "${API_UPLOAD_FILE}?folderid=$2&auth=${AUTH_ID}" \
   -F "filename=@$1" \
   --compressed
 }
+function get_file_public_link(){
+### $1 is the file id to share
+$CURL_BIN $CURL_OPT "${API_FILE_PUBLIC_LINK}?fileid=$1&auth=${AUTH_ID}" \
+  -H "Accept: application/json, text/javascript, */*; q=0.01" \
+  -H "Accept-Language: en-US,en;q=0.9,ar;q=0.8" \
+  -H "Connection: keep-alive" \
+  --compressed| $JQ_BIN -r '.link'
+}
+function list_public_links(){
+  $CURL_BIN $CURL_OPT "${API_LIST_PUBLIC_LINKS}?auth=${AUTH_ID}" \
+  -H "Accept: application/json, text/javascript, */*; q=0.01" \
+  -H "Accept-Language: en-US,en;q=0.9,ar;q=0.8" \
+  -H "Connection: keep-alive" \
+  --compressed|$JQ_BIN -r '.publinks[].link'
+}
 ##############################################
 ###########START##############################
 ##############################################
@@ -156,8 +173,8 @@ if [[ ${PIPESTATUS[0]} -ne 4 ]]; then
     exit 1
 fi
 
-LONGOPTS=list-root-dir,list-dir:,download-file:,upload-file:,folder-id:,help
-OPTIONS=lr:d:u:i:h
+LONGOPTS=list-root-dir,list-dir:,download-file:,upload-file:,folder-id:,share-file:,list-public-links,help
+OPTIONS=lr:d:u:i:s:Lh
 ! PARSED=$(getopt --options=$OPTIONS --longoptions=$LONGOPTS --name "$0" -- "$@")
 if [[ ${PIPESTATUS[0]} -ne 0 ]]; then
     exit 2
@@ -167,6 +184,10 @@ while true; do
     case "$1" in
         -l|--list-root-dir)
             pretty_list_root_folder
+            shift
+            ;;
+        -L|--list-public-links)
+            list_public_links
             shift
             ;;
         -r|--list-dir)
@@ -181,6 +202,10 @@ while true; do
         -u| --upload-file)
             upload_file $2 $4
             shift 4
+            ;;
+        -s| --share-file)
+            get_file_public_link $2 
+            shift 2
             ;;
         -h|--help)
             usage
